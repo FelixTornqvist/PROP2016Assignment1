2d8a65ee507c06647d3f059b6ad2b240f3f1571c
FelixTornqvist <felix.trnqvist@gmail.com>
2016-11-23 14:26:38 +0100

    playing a little bit

--- myObject.js
+++ myObject.js
@@ -1,78 +1,71 @@
-function myObject() {};
+var myObject = {
 
-var name = "myObject";
-
-var prototypes = [];
-myObject.prototypes = prototypes;
-
-myObject.create = function(prototypeList) {
+prototypes : [],
+	
+create : function(prototypeList) {
     var newObject = {};
     newObject.__proto__ = myObject;
-    if (prototypeList != null) {
+	
+    if (prototypeList != null && prototypeList.length > 0) {
         for(var newProto in prototypeList) {
+			print("added proto which is a: "+typeof newProto)
 
             if (this.prototypes.indexOf(newProto) == -1) {
                 this.prototypes.push(newProto);
-            } else {
-                print("not added to list");
             }
         }
     }
     return newObject;
-};
+},
 
 // myObject.call = myObject.bind(myObject);
 
-myObject.call = function(methodName, args) {
+call : function(methodName, args) {
 
     if (this.hasOwnProperty(methodName)) {
-        return "yes";
+        return "I have the method";
         // eval(methodName + "(" + args + ");");
 
     } else {
         if (this.prototypes.length > 0) {
-            print("hahhaa " + prototypes.length);
+            print("I have prototypes, amount: " + prototypes.length);
             for(var p in prototypes) {
-                print("index " + prototypes[p]);
-
-                print(p.hasOwnProperty("call"));
-                p.call(methodName, args);
+                print("prototype is a " + typeof p);
+                print("prototype have call method: "+p.hasOwnProperty("call"));
+                //p.call(methodName, args);
             }
         }
 
     }
 
 
-};
+}
 
-// 
+};
 
 
 
 var obj1 = myObject.create(null);
 obj1.name = "obj1";
-
 obj1.func = function(arg) {
     return "func1: " + arg;
 };
-
-print(obj1.func("korv"));
 
 var punkt = myObject.create(null);
 punkt.name = "punkt";
 punkt.x = 0;
 punkt.y = 0;
 
-// print(punkt.x + ":" + punkt.y);
+var protoList = [obj1, punkt];
+for(proto in protoList)
+	print(typeof proto);
 
-var punktKorv = myObject.create([obj1, punkt]);
+
+var punktKorv = myObject.create(protoList);
 punktKorv.name = "punktkorv"
 
-// punktKorv.func = function(arg) {
-//     return "punktKorv: " + arg;
-// };
-print(myObject.hasOwnProperty("call"));
-
-print("ibj1 hade call " + obj1.call("func", "hejsan"));
-print("punktkorv funkade " + punktKorv.call("func", "hejsan"));
+print("obj1 call:");
+print("obj1.call returnerar: " + obj1.call("func", "hejsan"));
+print("punktkorv call:");
+print("punktkorv.call returnerar: " + punktKorv.call("func", "hejsan"));
 //print(punktKorv.x);
