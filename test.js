070dcc8ac6f64593348ab7d73b9ba58c4f9fb3c7
Annika Svedin <annika.svedin@gmail.com>
2016-11-28 14:41:59 +0100

    finally

--- test.js
+++ test.js
@@ -2,7 +2,8 @@ function protoBlueprint(mother) {
 	var owner = mother;
 	var prototypes = [];
 	this.getPrototypes = function() {
-		return prototypes;
+
+		return prototypes.slice();
 	};
 
 	this.setPrototypes = function(newProtos) {
@@ -10,11 +11,8 @@ function protoBlueprint(mother) {
 		prototypes = newProtos;
 
 		if (this.protosContainsOwner(owner)) {
-			print("FORBIDDEN!!");		// should maybe use a throw statement instread
 			prototypes = oldProtos;
-			return false;
-		} else {
-			return true;
+			throw "You Shall Not PASS!      ...this prototype";
 		}
 	};
 
@@ -29,11 +27,9 @@ function protoBlueprint(mother) {
 			} else {
 				containsOwner = proto.prototypes.protosContainsOwner(superOwner);
 			}
-
 			if (containsOwner)
 				break;
 		}
-
 		return containsOwner;
 	}
 };
@@ -44,6 +40,7 @@ var myObject = {
 	prototypes : new protoBlueprint,
 
 	create : function(prototypeList) {
+
 		var newObject = Object.create(this);
 		var newProtoList = this.prototypes.getPrototypes().slice(); // .slice() to create a new array object
 
@@ -54,20 +51,18 @@ var myObject = {
  
 				if (newProtoList.indexOf(newProto) == -1) {
 						newProtoList.push(newProto);
-				} else {
-					print("prototype already added");
 				}
 			} 
 		}
 		newObject.prototypes = new protoBlueprint(newObject);
 		newObject.prototypes.setPrototypes(newProtoList);
+
 		return newObject;
 	},
 
-	call : function(methodName, args) {
-		if (methodName in this) {
-			//return this.name+" have the function";
-        	return eval("this." + methodName + "('" + args + "');");
+	call : function(funcName, parameters) {
+		if (funcName in this) {
+      return eval("this." + funcName + "('" + parameters + "');");
 		} else {
 			var myProtos = this.prototypes.getPrototypes();
 
@@ -75,10 +70,10 @@ var myObject = {
 				var currentProto = myProtos[i];
 
 	  			try {
-	  				var result = currentProto.call(methodName, args);
+	  				var result = currentProto.call(funcName, parameters);
 	  				return result;
-	  			}catch(exception){
-	  				if(exception == "could not find function"){
+	  			} catch (exception) {
+	  				if (exception == "could not find function") {
 	  					continue;
 	  				}
 	  				else 
@@ -90,36 +85,14 @@ var myObject = {
 	}
 };
 
+
 function createClass(className, superClassList) {
   var newClass = myObject.create(superClassList);
   newClass.name = className;
-  newClass.new = function() {
-    return new Object(this);
+  
+  newClass.new = function() { 
+  	return Object.create(this); 
   };
+  
   return newClass;
 };
-
-
-
-/*--- test code ---*/
-var class0 = createClass("Class0", null);
-class0.func = function(arg) { return "func0: " + arg; };
-var class1 = createClass("Class1", [class0]);
-var class2 = createClass("Class2", []);
-class2.func = function(arg) { return "func2: " + arg; };
-var class3 = createClass("Class3", [class1, class2]);
-var obj3 = class3.new();
-var result = obj3.call("func", ["hello"]);
-
-print("result " + result);
-
-class2.newFunk = function(arg){
-	return "class2 newFunk :"+arg;
-}
-print(obj3.call("newFunk", "call from obj3"));
-
-class0.prototypes.setPrototypes([class0]);
-class0.prototypes.setPrototypes([class1]);
-class0.prototypes.setPrototypes([class3]);
-class0.prototypes.setPrototypes([class2]);
-// class0.call("korv", [null]);
